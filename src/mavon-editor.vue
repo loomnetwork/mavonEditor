8560eb860d77330fc3703700f555e5e546a423c6
Jim Skogman <yim.apichai@gmail.com>
2018-03-05 18:14:45 +0700

    Add solidity support

--- src/mavon-editor.vue
+++ src/mavon-editor.vue
@@ -99,6 +99,7 @@
     import {toolbar_right_click} from './lib/toolbar_right_click.js'
     import {CONFIG} from './lib/config.js'
     import hljs from './lib/core/highlight.js'
+    import hljsDefineSolidity from './lib/core/solidity.js'
     import markdown from './lib/mixins/markdown.js'
 
     var s_md_toolbar_left = require('./components/s-md-toolbar-left.vue')
@@ -242,6 +243,8 @@
             $vm.initExternalFuc();
             // 初始化语言
             this.initLanguage();
+            hljsDefineSolidity(hljs);
+
             this.$nextTick(() => {
                 // 初始化Textarea编辑开关
                 this.editableTextarea();
